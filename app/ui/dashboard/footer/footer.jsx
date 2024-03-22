12ff7d9e18b84577ed5b848b64127ce892fc61db
n1dll <ngeorgiev89@outlook.com>
2024-03-22 17:30:53 +0200

    first commit

---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>n1dll</div>
+      <div className={styles.text}>© All rights reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
